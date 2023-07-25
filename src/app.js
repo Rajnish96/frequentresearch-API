319f4617dca7b90ee26d9db0376b858492371594
Rajnish96 <rajnishkumarsingh93@gmail.com>
2023-07-25 20:47:39 +0530

    completed

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 require('./config/config')
 
 const port = 5000
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const users = require('./routes/userRouter')
 
